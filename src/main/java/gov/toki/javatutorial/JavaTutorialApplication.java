67d0bede5cb8b22d87ab077ae0c2f142c2279fcf
yildirim-murat <muratyildirim.work@gmail.com>
2024-02-26 09:41:05 +0300

    Initial commit

---
+++ src/main/java/gov/toki/javatutorial/JavaTutorialApplication.java
@@ -0,0 +1,13 @@
+package gov.toki.javatutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTutorialApplication.class, args);
+    }
+
+}
