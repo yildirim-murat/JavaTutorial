63e2d86bdff0227f05d7eb93b0fcb048c7489bbc
yildirim-murat <muratyildirim.work@gmail.com>
2024-02-26 09:41:05 +0300

    Initial commit

---
+++ src/test/java/gov/toki/javatutorial/JavaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.toki.javatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
